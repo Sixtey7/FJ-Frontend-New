43b286c4f9dae2ade382f56fd6ec2961755e9cbb
Patrick Shaw <sixtey7@gmail.com>
2020-07-30 21:29:26 -0600

    start adding an account component

---
+++ components/accounts/AccountList.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-card
+        class = "mx-auto"
+        max-width="300px"
+        title
+    >
+        <v-list dense>
+            <v-subheader>Accounts</v-subheader>
+            <v-list-item-group
+                v-model="account"
+                color="primary"
+            >
+                <v-list-item
+                    v-for = "(account, i) in accounts"
+                    :key=i
+                >
+                    <v-list-item-content>
+                        <v-list-item-title v-text="account.name"></v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list-item-group>
+        </v-list>
+    </v-card>
+</template>
