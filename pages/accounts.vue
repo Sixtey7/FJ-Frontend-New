72fac26cb8b16f6ea1b8c7e9367b639605788c3f
Patrick Shaw <sixtey7@gmail.com>
2020-08-05 22:03:28 -0600

    pull in the accounts list

--- pages/accounts.vue
+++ pages/accounts.vue
@@ -18,10 +18,14 @@
   </v-layout>
 </template>
 <script>
-import AccountModel from '../../model/AccountModel';
+import AccountModel from '../model/AccountModel';
+import AccountList from '../components/AccountList';
 import Vue from 'vue';
 export default {
     name: 'Accounts',
+    components: {
+      AccountList
+    },
     data() {
         return {
             accountModel:  new AccountModel(Vue.$log, this.backendHost)
