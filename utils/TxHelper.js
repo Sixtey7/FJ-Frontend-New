8f8bcfc5c2162a31cf0e580b469707ebb5ad6649
Patrick Shaw <sixtey7@gmail.com>
2020-08-17 20:26:21 -0600

    comments are cool

--- utils/TxHelper.js
+++ utils/TxHelper.js
@@ -1,7 +1,6 @@
 /**
  * Class used to assist with working with Transaction objects on the frontend
  */
-
  class TxHelper {
     _logger;
 
@@ -9,13 +8,17 @@
         this._logger = logger;
     }
 
+    /**
+     * Prepares the provided transactions to be handled by the frontend
+     * @param {Array} txList array of Transactions to be massaged
+     */
     massageTransactions(txList) {
         if (typeof(txList) !== 'undefined') {
             txList.foreach(transaction => {
                 transaction.date = new Date(transaction.date)
             });
 
-            transactionList.osrt(this._compareTx);
+            transactionList.sort(this._compareTx);
         }
 
         this._calculateBalance(txList);
@@ -23,6 +26,11 @@
         return txList;
     }
 
+    /**
+     * Method used to compare two transactions for the purposes of ordering
+     * @param {Transaction} a the first transaction to be compared
+     * @param {Transaction} b The second transaction to be compared
+     */
     _compareTx(a, b) {
         if (a.date < b.date) {
             return -1;
@@ -34,6 +42,10 @@
         return 0;
     }
 
+    /**
+     * Calculates the balance for each transaction of the provided sorted list
+     * @param {Array} txList array of Transaction objects
+     */
     _calculateBalance(txList) {
         let currentBalance = 0;
         txList.foreach(transaction => {
