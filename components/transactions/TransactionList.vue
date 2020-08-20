784dcd7a5e328e871fe9eca4139cfaf8da098a15
Patrick Shaw <sixtey7@gmail.com>
2020-08-19 22:07:22 -0600

    add the initial tx list

---
+++ components/transactions/TransactionList.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-card
+        class = "mx-auto"
+        max-width="300px"
+        title
+    >
+        <v-data-table
+            :headers="headers"
+            :items="txArray"
+            :items-per-page="50"
+            class="elevation-1"
+        >
+        </v-data-table>
+    </v-card>
+</template>
+<script>
+import TransactionModel from '../../model/TransactionModel';
+export default {
+    name: 'TransactionList',
+    data() {
+        return {
+            headers: [
+                {
+                    text: 'Name',
+                    align: 'start',
+                    sortable: true,
+                    value: 'name'
+                },
+                {
+                    text: 'Amount',
+                    value: 'amount'
+                },
+                {
+                    text: 'Notes',
+                    value: 'notes'
+                }
+            ]
+        }
+    },
+    props: {
+        transactionModel: TransactionModel,
+        txArray: Array
+    }
+}
+</script>
