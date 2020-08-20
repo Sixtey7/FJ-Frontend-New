9717828ffa0d4cd1e9fe4f80d9c3e4bbb9fba68c
Patrick Shaw <sixtey7@gmail.com>
2020-08-20 06:54:38 -0600

    actually use all the transaction stuff

--- pages/transactions.vue
+++ pages/transactions.vue
@@ -1,19 +1,27 @@
 <template>
   <v-layout>
     <v-flex class="text-center">
-      <img
-        src="/v.png"
-        alt="Vuetify.js"
-        class="mb-5"
-      >
-      <blockquote class="blockquote">
-        Transactions will go here!
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
+      <TransactionList
+        :transactionModel = "transactionModel"
+        :txArray = "transactionModel.txArray" />
     </v-flex>
   </v-layout>
 </template>
+<script>
+import TransactionModel from '../model/TransactionModel';
+import TransactionList from '../components/transactions/TransactionList';
+export default {
+  name: 'Transactions',
+  components: {
+    TransactionList 
+  },
+  data() {
+    return {
+      transactionModel = new TransactionModel(this.backendHost)
+    }
+  },
+  beforeCreate() {
+    this.backendHost = "raspberrypi:8081"
+  }
+}
+</script>
